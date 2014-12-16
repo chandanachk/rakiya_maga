518b247a352e583eecd546c3368a51c3deb4779d
Chandana <chandanachk@gmail.com>
2014-12-16 15:48:46 +0530

    initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=rakiya_maga',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
